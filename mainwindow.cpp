a445a66ecbc63e5b701fa57f8bfc9f23b63c0bb1
Kathryn Sheahen <112595660+ksheahen@users.noreply.github.com>
2024-08-21 14:59:56 -0500

    Added UI for front page and vegetable page.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,23 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->stackedWidget->setCurrentIndex(0);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnMainVegetables_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
